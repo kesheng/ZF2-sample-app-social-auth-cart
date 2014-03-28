057ec917dade485f761f26aac6f3f52dcca9c88b
ke sheng <xxc@xiaoxitekiMacBook-Pro.local>
2014-03-28 16:02:15 +0000

    update for DoctrineORM 0.8 An alias “zfcuser_doctrine_em”

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -105,6 +105,7 @@ return array(
         ),
         'aliases' => array(
             'translator' => 'MvcTranslator',
+            'zfcuser_doctrine_em' => 'Doctrine\ORM\EntityManager',
         ),
     ),
     'translator' => array(
